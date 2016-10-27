b3ae11301be04d259550555d2c9bed49f03b29f7
Ben Ratliff <bratliff@amplify.com>
2016-10-27 15:02:02 -0400

    removed compass from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 // modules
 var assemble = require('fabricator-assemble');
 var browserSync = require('browser-sync');
-//var compass = require('gulp-compass');
 var cheerio = require('gulp-cheerio');
 var csso = require('gulp-csso');
 var del = require('del');
